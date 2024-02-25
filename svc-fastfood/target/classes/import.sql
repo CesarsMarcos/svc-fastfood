3bd91b604bb7f81b659b6a222d791f204e675680
César Marcos <39070231+CesarsMarcos@users.noreply.github.com>
2024-02-24 22:43:34 -0500

    primer-commit
    primer-commit

---
+++ svc-fastfood/target/classes/import.sql
@@ -0,0 +1,8 @@
+insert into tb_categorias (descripcion,is_activo) values ('plato criollo',1);
+
+insert into tb_tiendas (descripcion,numero,piso,seccion,is_activo) values ('oeshle','150','2','ropa',1);
+
+insert into tb_platos (nombre,descripcion,imagen,precio,is_activo,is_menu_dia,categoria_id) values ('Lomo','Lomo Saltado','src/images/lomo',8.5,1,1,1);
+
+insert into tb_usuarios (nombres,apellidos,celular,username,password,is_activo,tienda_id) values ('cesar','marcos',123456789,'cmarcos','12345',1,1);
+
